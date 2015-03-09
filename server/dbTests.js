c6e976b0233dd074f58d021c64bed0f2c10984d9
Bryan Watts <bryan.watts@bw101904ml.local>
2015-03-08 21:51:11 -0400

    create database wrapper for file io

---
+++ server/dbTests.js
@@ -0,0 +1,16 @@
+var database = require('./database');
+var volumes = require('./volumes');
+
+var db = database.connect();
+volumes.ensureExist(db);
+
+var test = {
+    name: 'grifter',
+    description: 'the best at grifting',
+    comicVine: 123123123
+};
+
+volumes.addRow(db, test);
+volumes.printAll(db);
+
+db.disconnect();
