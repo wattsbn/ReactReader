b713e8e2f68c54f4e35cb4610c0f3f12f3dd2e49
Bryan Watts <bryan.watts@bw101904ml.local>
2015-04-25 19:38:22 -0400

    Add generation task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,75 @@
 'use strict';
-var application = require('./utilities/webpack/application');
 
-module.exports = function(grunt) {
-    // Project Configuration
-    grunt.initConfig({
-        connect: {
-            server: {
-                options: {
-                    port: 9000,
-                    hostname: '*',
-                    keepalive: true
-                }
-            }
-        },
-        jshint: {
-            options: {
-                additionalSuffixes: ['.js'],
-                esnext: true,
-                jshintrc: '.jshintrc'
-            },
-            files: [
-                '*.js',
-                '**/*.js',
-                '!build/**/*',
-                '!**/node_modules/**/*'
-            ]
-        },
-        clean: {
-            build: ['./build']
-        }
-    });
-
-    // *** Load plugins *** //
+function loadConnect(grunt, config) {
     grunt.loadNpmTasks('grunt-contrib-connect');
+    config.connect = {
+        server: {
+            options: {
+                port: 9000,
+                hostname: '*',
+                keepalive: true
+            }
+        }
+    };
+}
+
+function loadLint(grunt, config) {
     grunt.loadNpmTasks('grunt-jsxhint');
+    config.jshint = {
+        options: {
+            additionalSuffixes: ['.js'],
+            esnext: true,
+            jshintrc: '.jshintrc'
+        },
+        files: [
+            '*.js',
+            '**/*.js',
+            '!build/**/*',
+            '!**/node_modules/**/*'
+        ]
+    };
+}
+
+function loadClean(grunt, config) {
     grunt.loadNpmTasks('grunt-contrib-clean');
+    config.clean = {
+        build: ['./build'],
+        data: ['./data']
+    }
+}
 
-    // *** Load webpack tasks *** //
-    function toBool(value) { return !(!value || value === 'false'); }
-    grunt.registerTask('webpack-build', 'Build the application using webpack', function(minify, hideSourceMaps) {
-        application.build(toBool(minify), toBool(hideSourceMaps), this.async());
+function loadHelpers(grunt) {
+    grunt.registerTask('data', 'Generates files for use during development', function() {
+        require('./utilities/helpers/generateData').createComics();
     });
+}
 
-    grunt.registerTask('fast-build', 'Build the application without minification', ['webpack-build']);
-    grunt.registerTask('slow-build', 'Build the application with minification', ['webpack-build:true']);
-    grunt.registerTask('build', 'Clean and build the application', ['clean:build', 'webpack-build:true']);
+function toBool(value) { return !(!value || value === 'false'); }
+function loadWebpack(grunt) {
+    var application = require('./utilities/webpack/application');
 
     grunt.registerTask('webpack-connect', 'Start webpack dev server', function(fast, auto, minify) {
         application.connect(toBool(fast), toBool(auto), toBool(minify), this.async());
     });
+
+    grunt.registerTask('webpack-build', 'Build the application using webpack', function(minify, hideSourceMaps) {
+        application.build(toBool(minify), toBool(hideSourceMaps), this.async());
+    });
+}
+
+module.exports = function(grunt) {
+    var config = {};
+
+    loadConnect(grunt, config);
+    loadLint(grunt, config);
+    loadClean(grunt, config);
+
+    loadHelpers(grunt);
+    loadWebpack(grunt);
+
+    grunt.registerTask('fast-build', 'Build the application without minification', ['webpack-build']);
+    grunt.registerTask('slow-build', 'Build the application with minification', ['webpack-build:true']);
+    grunt.registerTask('build', 'Clean and build the application', ['clean:build', 'webpack-build:true']);
 
     grunt.registerTask('start', 'Start webpack dev server on fast mode', ['webpack-connect:true:true:false']);
     grunt.registerTask('start-manual', 'Start webpack dev server on fast mode without auto refresh turned on', ['webpack-connect:true:false:false']);
@@ -59,4 +80,6 @@ module.exports = function(grunt) {
 
     grunt.registerTask('lint', ['jshint']);
     grunt.registerTask('default', ['lint', 'build']);
+
+    grunt.initConfig(config);
 };
---
+++ utilities/helpers/generateData.js
@@ -0,0 +1,40 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var _ = require('underscore');
+
+var comicData = {
+    'iron man': null,
+    'new 52': {
+        'batman': null,
+        'the flash': null
+    }
+};
+
+function createVolume(basePath, name) {
+    _.each(_.range(1, _.random(5, 10)), function(index) {
+        fs.closeSync(fs.openSync(path.resolve(basePath, name + ' ' + index + '.cbr'), 'w'));
+    });
+}
+
+function createData(data, basePath, createFn) {
+    mkdirp.sync(basePath);
+    _.each(data, function(value, key) {
+        if (value) {
+            createData(value, path.resolve(basePath, key));
+        } else {
+            var directory = path.resolve(basePath, key);
+            mkdirp.sync(directory);
+            createFn(directory, key);
+        }
+    });
+}
+
+function createComics() {
+    createData(comicData, path.resolve(__dirname, '..', '..', 'data'));
+}
+
+module.exports = {
+    createComics: createComics
+};
